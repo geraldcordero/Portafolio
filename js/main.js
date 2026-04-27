4c98d7ff956ef97a96679c32964db4bb1d7415d4
geraldcordero <gerald.cordero.dev+github@gmail.com>
2026-04-27 08:36:15 -0600

    fixed navbar

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@ let container2 = document.getElementById("pj2");
 let containerOp2 = document.getElementById("pj2Options");
 
 function switchOpstions1() {
-    container1.style.zIndex=1;
-    containerOp1.style.display='block';
+    container1.style.zIndex = 1;
+    containerOp1.style.display = 'block';
 }
 function switchInfo1() {
-    container1.style.zIndex=2;
-    containerOp1.style.display='none';
+    container1.style.zIndex = 2;
+    containerOp1.style.display = 'none';
 }
 
 function switchOpstions2() {
-    container2.style.zIndex=1;
-    containerOp2.style.display='block';
+    container2.style.zIndex = 1;
+    containerOp2.style.display = 'block';
 }
 function switchInfo2() {
-    container2.style.zIndex=2;
-    containerOp2.style.display='none';
+    container2.style.zIndex = 2;
+    containerOp2.style.display = 'none';
 }
 
 const open = document.getElementById("open");
@@ -34,3 +34,8 @@ close.addEventListener('click', () => {
     nav.classList.remove('header__list--visible');
     nav.classList.add('header__list--hidden');
 });
+
+function handlerNavOptions() {
+    nav.classList.remove('header__list--visible');
+    nav.classList.add('header__list--hidden');
+}
